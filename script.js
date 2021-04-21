30acaf379d4280476d3bf703c2a06352ecd1c97b
Justin Elmore <jelmore1674@pm.me>
2021-04-21 05:04:05 -0500

    Working on rewriting code to work on calculating the score better

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 /** @format */
-
+let playerScore = 0;
+let computerScore = 0;
 let game = ["Rock", "Paper", "Scissors"];
 
 function computerPlay(game) {
     return game[Math.floor(Math.random() * game.length)];
 }
-
 // console.log(computerPlay(game));
-function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+function capitalize(playerSelection) {
+    return (
+        playerSelection.charAt(0).toUpperCase() +
+        playerSelection.slice(1).toLowerCase()
+    );
 }
 
-string = playerSelection;
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay(game);
+    var playerSelection = capitalize(playerSelection);
     if (computerSelection === "Rock" || "Paper" || "Scissors") {
         if (computerSelection === playerSelection) {
             return "The Result is a Tie!";
@@ -21,50 +24,41 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "Rock") {
             if (playerSelection === "Paper") {
                 return `You Win! ${playerSelection} beats ${computerSelection}!`;
-            } else {
-                return `You lose ${computerSelection} beats ${playerSelection}!`;
             }
         }
         if (computerSelection === "Paper") {
             if (playerSelection === "Scissors") {
                 return `You win! ${playerSelection} beats ${computerSelection}`;
-            } else {
-                return `You lose ${computerSelection} beats ${playerSelection}!`;
             }
         }
         if (computerSelection === "Scissors") {
             if (playerSelection === "Rock") {
                 return `You win! ${playerSelection} beats ${computerSelection}`;
-            } else {
-                return `You lose ${computerSelection} beats ${playerSelection}!`;
             }
+        }
+        if (computerSelection === "Paper" && playerSelection === "Rock") {
+            return (
+                `You Lose! ${computerSelection} beats ${playerSelection}!`,
+                computerScore++
+            );
+        }
+        if (computerSelection === "Rock" && playerSelection === "Scissors") {
+            return (
+                `You Lose! ${computerSelection} beats ${playerSelection}!`,
+                computerScore++
+            );
+        }
+        if (computerSelection === "Scissors" && playerSelection === "Paper") {
+            return (
+                `You Lose! ${computerSelection} beats ${playerSelection}!`,
+                computerScore++
+            );
         }
     }
 }
 
-function fullGame(playerSelect) {
-    let playerSelection = capitalize(playerSelect);
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
-    playRound(playerSelection, computerSelection);
-}
-let playerScore = 0;
-let computerScore = 0;
+let playerSelection = "rock";
+playRound(playerSelection);
+console.log(playRound(playerSelection));
 
-function winner(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        return "Good Job! You win the game!";
-    }
-    if (computerScore > playerScore) {
-        return "Try again. You lost to the computer";
-    }
-    if (playerScore === computerScore) {
-        return "Too bad, you didn't win or lose.";
-    }
-}
-var playerSelection = "Rock";
-var computerSelection = game;
-fullGame();
 console.log("Player:", playerScore, "Computer:", computerScore);
-console.log(winner(playerScore, computerScore));
