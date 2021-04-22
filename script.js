f89015c2988ce7ffcb0325e4bee8ff426dcc8e44
Justin Elmore <jelmore1674@pm.me>
2021-04-22 18:14:13 -0500

    Trying different things

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 /** @format */
-let playerScore = 0;
-let computerScore = 0;
+
 let game = ["Rock", "Paper", "Scissors"];
 
 function computerPlay(game) {
@@ -15,58 +14,51 @@ function capitalize(playerSelection) {
 }
 
 function playRound(playerSelection) {
-    let computerSelection = computerPlay(game);
+    var computerSelection = computerPlay(game);
     var playerSelection = capitalize(playerSelection);
-    if (computerSelection === "Rock" || "Paper" || "Scissors") {
-        if (computerSelection === playerSelection) {
-            return "The Result is a Tie!";
-        }
-        if (computerSelection === "Rock") {
-            if (playerSelection === "Paper") {
-                return `You Win! ${playerSelection} beats ${computerSelection}!`;
-            }
-        }
-        if (computerSelection === "Paper") {
-            if (playerSelection === "Scissors") {
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-            }
-        }
-        if (computerSelection === "Scissors") {
-            if (playerSelection === "Rock") {
-                return `You win! ${playerSelection} beats ${computerSelection}`;
-            }
-        }
-        if (computerSelection === "Paper") {
-            if (playerSelection === "Rock") {
-                return (
-                    `You Lose! ${computerSelection} beats ${playerSelection}!`,
-                    lose()
-                );
-            }
-        }
-        if (computerSelection === "Rock") {
-            if (playerSelection === "Scissors") {
-                return (
-                    `You Lose! ${computerSelection} beats ${playerSelection}!`,
-                    lose()
-                );
-            }
-        }
-        if (computerSelection === "Scissors") {
-            if (playerSelection === "Paper") {
-                lose();
-                return `You Lose! ${computerSelection} beats ${playerSelection}!`;
-            }
-        }
+    if (computerSelection === playerSelection) return "The Result is a Tie!";
+
+    if (computerSelection === "Scissors" && playerSelection === "Paper")
+        return "You lose! Scissors beats Paper";
+    if (computerSelection === "Scissors" && playerSelection === "Rock")
+        return "You Win! Rock beats Scissors";
+    if (computerSelection === "Paper" && playerSelection === "Rock")
+        return "You lose! Paper beats Rock";
+    if (computerSelection === "Paper" && playerSelection === "Scissors")
+        return "You Win! Scissors beats Paper";
+    if (computerSelection === "Rock" && playerSelection === "Scissors")
+        return "You lose! Rock beats Scissors";
+    if (computerSelection === "Rock" && playerSelection === "Paper")
+        return "You Win! Paper beats Rock";
+}
+
+function fullGame() {
+    var win = "win";
+    var loss = "lose";
+    var tie = "tie";
+
+    playRound(playerSelection);
+    r1 = playRound(playerSelection);
+    if (r1.includes(win)) {
+        playerScore++;
+        return r1;
+    } else if (r1.includes(loss)) {
+        computerScore++;
+        return r1;
+    } else if (r1.includes(tie)) {
+        return r1;
     }
+
+    playRound(playerSelection);
+    playRound(playerSelection);
+    playRound(playerSelection);
+    playRound(playerSelection);
 }
 
-function lose() {
-    computerScore++;
-}
-
-let playerSelection = "rock";
-playRound(playerSelection);
-console.log(playRound(playerSelection));
-
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection = "paper";
+fullGame();
+console.log(fullGame());
+console.log(playerSelection);
 console.log("Player:", playerScore, "Computer:", computerScore);
